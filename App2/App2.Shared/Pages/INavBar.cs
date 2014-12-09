4721eed75e0a4f17fbb6a249631a55f88a46844b
Alexander <duff1994@gmail.com>
2014-12-10 00:19:08 +0200

    Licensing

--- App2/App2.Shared/Pages/INavBar.cs
+++ App2/App2.Shared/Pages/INavBar.cs
@@ -1,4 +1,22 @@
-﻿using Windows.UI.Xaml;
+﻿/*
+ * Excellent - Student Organizer
+ * Copyright (c ) 2014, Alexander Davydov, All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3.0 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.
+ */
+
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Input;
 
 namespace App2.Pages
